d62b5f88de3758cb87ec9947a46135fd2a939c34
Bartłomiej Czajka <52079500+Resmakor@users.noreply.github.com>
2022-12-10 00:47:11 +0100

    Check resolution
    Function Sprawdz_rozdzielczosc (Check_resolution) added

--- Symulacja.cpp
+++ Symulacja.cpp
@@ -1,17 +1,29 @@
 #include "Symulacja.h"
-
-void Symulacja::Sprawdz_rozdzielczosc(int liczba, int rozmiar)
+#include <Windows.h>
+bool Symulacja::Sprawdz_rozdzielczosc(int liczba, int rozmiar)
 {
 	int szerokosc = sf::VideoMode::getDesktopMode().width;
 	int wysokosc = sf::VideoMode::getDesktopMode().height;
+	int pomnozone = liczba * rozmiar;
+	return pomnozone < szerokosc && pomnozone < wysokosc && pomnozone > 200;
 }
 void Symulacja::Symuluj()
 {
+	while (true)
+	{
 		int rozmiar_komorki, liczba_komorek;
 		std::cout << "Podaj liczbe komorek: ";
 		std::cin >> liczba_komorek;
 		std::cout << "Podaj rozmiar komorki: ";
 		std::cin >> rozmiar_komorki;
-		Plansza plansza(liczba_komorek, rozmiar_komorki);
-		plansza.Inicjalizuj();
+		if (this->Sprawdz_rozdzielczosc(liczba_komorek, rozmiar_komorki))
+		{
+			Plansza plansza(liczba_komorek, rozmiar_komorki);
+			plansza.Inicjalizuj();
+			break;
+		}
+		system("cls");
+		std::cout << "Okno dla poprzednich parametrow jest zbyt duze/male!" << std::endl;
+		
+	}
 }
