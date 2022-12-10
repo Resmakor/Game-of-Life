12303114669004424ff511d1e14310ab63436271
Bartłomiej Czajka <52079500+Resmakor@users.noreply.github.com>
2022-12-10 01:05:10 +0100

    Update Plansza.h
    Population array added

--- Plansza.h
+++ Plansza.h
@@ -6,6 +6,7 @@ class Plansza
 	const int liczba_komorek = { 15 };
 	const int rozmiar_komorki = { 30 };
 	sf::Vector2f vector;
+	bool populacja[];
 public:
 	Plansza(int ile_komorek, int jaki_rozmiar_komorki);
 	void Inicjalizuj();
