be6499e9b94c255a522d4ef468131d138fd02e14
Bartłomiej Czajka <52079500+Resmakor@users.noreply.github.com>
2022-12-10 14:16:46 +0100

    Current, next population and Destructor
    - Two 2D dynamic bool arrays added populacja_obecna (current population) and populacja_nastepna (next population)
    - These arrays are filled with false value
    - Destructor of these two arrays

--- Plansza.cpp
+++ Plansza.cpp
@@ -3,6 +3,30 @@ Plansza::Plansza(int ile_komorek, int jaki_rozmiar_komorki) : liczba_komorek(ile
 {
    vector.x = rozmiar_komorki;
    vector.y = rozmiar_komorki;
+   populacja_obecna = new bool* [liczba_komorek];
+   populacja_nastepna = new bool* [liczba_komorek];
+   for (int i = 0; i < liczba_komorek; i++)
+   {
+       populacja_obecna[i] = new bool[liczba_komorek];
+       memset(populacja_obecna[i], false, liczba_komorek * sizeof(bool));
+       populacja_nastepna[i] = new bool[liczba_komorek];
+       memset(populacja_nastepna[i], false, liczba_komorek * sizeof(bool));
+   }
+}
+
+Plansza::~Plansza()
+{
+    for (int i = 0; i < liczba_komorek; ++i)
+    {
+        delete[] populacja_obecna[i];
+        delete[] populacja_nastepna[i];
+    }
+    delete[] populacja_obecna;
+    delete[] populacja_nastepna;
+}
+
+void Plansza::Wyswietl_komorke()
+{
 }
 
 void Plansza::Inicjalizuj()
