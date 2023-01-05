a305ddb4b16ba3890f035854f3f2bf2baa659576
Bartłomiej Czajka <52079500+Resmakor@users.noreply.github.com>
2023-01-05 13:31:19 +0100

    Safety++; :+1:
    Menu updated, safety added

--- Symulacja.cpp
+++ Symulacja.cpp
@@ -13,7 +13,33 @@ int Symulacja::Zalecany_rozmiar_komorki(int liczba_komorek)const
 {
 	int min = std::min(sf::VideoMode::getDesktopMode().width, sf::VideoMode::getDesktopMode().height);
 	min *= 0.9;
+	if (liczba_komorek < 2)
+	{
+		return 0;
+	}
 	return  min / liczba_komorek;
+}
+
+
+void Symulacja::Wyswietl_sterowanie()const
+{
+	system("cls");
+	std::cout << "================================= STEROWANIE =================================" << std::endl;
+	std::cout << std::endl;
+	std::cout << "	ABY OZYWIC/ZABIC KOMORKE NACISNIJ NA NIA LEWYM PRZYCISKIEM MYSZY W CZASIE PAUZY" << std::endl;
+	std::cout << "	ABY ZATRZYMAC/WZNOWIC SYMULACJE WCISNIJ KLAWISZ 'P'" << std::endl;
+	std::cout << "	ABY ZWIEKSZYC/ZMNIEJSZYC OPOZNIENIE WCISNIJ KLAWISZ STRZALKA GORNA/DOLNA" << std::endl;
+	std::cout << "===============================================================================" << std::endl;
+}
+
+void Symulacja::Wczytaj(int &parametr)
+{
+	while (!(std::cin >> parametr))
+	{
+		std::cin.clear();
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		std::cout << "	Podaj inna wartosc: ";
+	}
 }
 
 Symulacja::Symulacja()
@@ -21,27 +47,32 @@ Symulacja::Symulacja()
 	std::cout << "================================= GRA W ZYCIE =================================" << std::endl;
 	std::cout << std::endl;
 	std::cout << "	Podaj liczbe komorek w rzedzie (zalecana liczba 3 - 200): ";
-	std::cin >> liczba_komorek;
-	rozmiar_komorki = Zalecany_rozmiar_komorki(liczba_komorek);
-	if (this->Sprawdz_rozdzielczosc(liczba_komorek, rozmiar_komorki) && rozmiar_komorki > 1)
+	Wczytaj(liczba_komorek);
+	while (liczba_komorek < 2 || Zalecany_rozmiar_komorki(liczba_komorek) < 2)
 	{
-		std::cout << "	Podaj ile zyjacych komorek wylosowac: (0 - " << liczba_komorek * liczba_komorek << "): ";
-		std::cin >> komorki_do_wylosowania;
-		std::cout << "	Podaj opoznienie miedzy kolejnymi ruchami (domyslnie 100 ms): ";
-		std::cin >> opoznienie_miedzy_ruchami;
-		system("cls");
-		std::cout << "================================= STEROWANIE =================================" << std::endl;
-		std::cout << std::endl;
-		std::cout << "	ABY OZYWIC/ZABIC KOMORKE NACISNIJ NA NIA LEWYM PRZYCISKIEM MYSZY W CZASIE PAUZY" << std::endl;
-		std::cout << "	ABY ZATRZYMAC/WZNOWIC SYMULACJE WCISNIJ KLAWISZ 'P'" << std::endl;
-		std::cout << "	ABY ZWIEKSZYC/ZMNIEJSZYC OPOZNIENIE WCISNIJ KLAWISZ STRZALKA GORNA/DOLNA" << std::endl;
-		std::cout << "===============================================================================" << std::endl;
+		std::cout << "	Podaj inna wartosc: ";
+		Wczytaj(liczba_komorek);
 	}
-	
+	rozmiar_komorki = Zalecany_rozmiar_komorki(liczba_komorek);
+	std::cout << "	Podaj ile zyjacych komorek wylosowac: (0 - " << liczba_komorek * liczba_komorek << "): ";
+	Wczytaj(komorki_do_wylosowania);
+	while (komorki_do_wylosowania < 0 || komorki_do_wylosowania > liczba_komorek * liczba_komorek)
+	{
+		std::cout << "	Podaj inna wartosc: ";
+		Wczytaj(liczba_komorek);
+	}
+	std::cout << "	Podaj opoznienie miedzy kolejnymi ruchami (domyslnie 100 ms): ";
+	Wczytaj(opoznienie_miedzy_ruchami);
+	while (opoznienie_miedzy_ruchami < 0)
+	{
+		std::cout << "	Podaj inna wartosc: ";
+		Wczytaj(opoznienie_miedzy_ruchami);
+	}
 }
 
 void Symulacja::Symuluj()
 {
+	Wyswietl_sterowanie();
 	Plansza plansza(liczba_komorek, rozmiar_komorki, opoznienie_miedzy_ruchami);
 	plansza.Wybierz_i_losowych_pol(komorki_do_wylosowania);
 	plansza.Inicjalizuj();		
--- Symulacja.h
+++ Symulacja.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "Plansza.h"
+#include <limits>
 
 class Symulacja
 {
@@ -8,6 +9,8 @@ class Symulacja
 	int komorki_do_wylosowania;
 	int opoznienie_miedzy_ruchami;
 public:
+	void Wyswietl_sterowanie()const;
+	void Wczytaj(int &parametr);
 	bool Sprawdz_rozdzielczosc(int liczba, int rozmiar)const;
 	int Zalecany_rozmiar_komorki(int liczba_komorek)const;
 	void Symuluj();
